39a9057b79f7072f82e1292d2c3e595543e2d807
unknown <rostik057@gmail.com>
2024-03-04 20:06:29 +0300

    !Fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CategorySeeder::class,
             UserSeeder::class,
         ]);
 
